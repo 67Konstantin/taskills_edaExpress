70ee1c6b5d6917d3ced38543c610429e9c62825c
dozy-owl <97381427+dozy-owl@users.noreply.github.com>
2023-12-08 00:24:44 +0300

    Update shoping_basket.dart

--- lib/features/mainscreen/view/shoping_basket.dart
+++ lib/features/mainscreen/view/shoping_basket.dart
@@ -10,11 +10,37 @@ class ShopingScreen extends StatefulWidget {
 }
 
 class _ShopingScreenState extends State<ShopingScreen> {
+
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.amber,
-      body: Text('Привет'),
+    TabController _tabController = TabController(length: 2, vsync: this);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Корзина')),
+      body: Center(
+        child: ListView(
+          children: [
+            Container(
+              width: double.maxFinite,
+              height: foodName.length * 60,
+              child: ListView.builder(
+                    //physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: MyVariables().basketName.length,
+                      itemBuilder: (context,index){
+                        return  ListTile(
+                          title: Text('${MyVariables().basketName[index]}'),
+                          leading: Image.asset(foodImages[index], height: 40, width: 40,),
+                          subtitle: Text(MyVariables().basketPrice[index]),
+                          isThreeLine: true,
+                        );
+                      }),
+            ),
+
+          ],
+        )
+      ),
     );
   }
 }
